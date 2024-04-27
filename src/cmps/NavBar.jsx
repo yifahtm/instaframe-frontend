98b6bd9681a062f67a4c3a06366c965641807aa4
Yifaht Meir <yifahtmyers@gmail.com>
2024-04-27 12:27:35 +0300

    Add nav bar mini&style

--- src/cmps/NavBar.jsx
+++ src/cmps/NavBar.jsx
@@ -8,10 +8,56 @@ import { CreateModal } from './CreateModal.jsx'
 
 export function NavBar() {
     const user = useSelector(storeState => storeState.userModule.watchedUser)
+    const newMessage = useSelector(storeState => storeState.messageModule.newMessage)
     const isModalOpen = useSelector(storeState => storeState.systemModule.isModalOpen)
     const [full, setFull] = useState(true)
 
     console.log(user)
+
+    function onMessages() {
+        setFull(!full)
+    }
+
+    function onFull() {
+        setFull(true)
+    }
+
+    // function onSearch() {
+    //     setSearchModal(!searchModal)
+    //     setFull(!full)
+    // }
+    // function onNotifications() {
+    //     // setSearchModal(!searchModal)
+    //     notificationsModal(!notifications)
+    //     gotNewNotification(false)
+    //     setFull(!full)
+    // }
+    // async function onLogout() {
+    //     try {
+    //         await logout()
+    //         console.log('USER FROM LOGOUT')
+
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+    // const onReciveNewActivity = (activity) => {
+    //     setActivityNotif(prevActivity => [...prevActivity, activity])
+    //     // activityNotif.unshift(activity)
+    //     console.log('ACTIVITY', activity)
+    //     gotNewNotification(true)
+    // }
+
+    // useEffect(() => {
+    //     socketService.on('new-reacting-activity', onReciveNewActivity)
+
+    //     return () => {
+    //         socketService.off('new-reacting-activity', onReciveNewActivity)
+    //     }
+    // }, [])
+
+
+    // if (!user) return <div className="loading-page"></div>
 
     // async function onLogout() {
     //     try {
@@ -33,21 +79,21 @@ export function NavBar() {
     return (
         <>
 
-            <section className={full ? "nav-bar-full" : "nav-bar-mini"}>
+            <section className={full ? "nav-bar" : "nav-bar mini"}>
                 <a href='/'>
-                    {full ? <h1 className="logo">Instaframe</h1> : null}
+                    {full ? <h1 onClick={onFull} className="logo">Instaframe</h1> : null}
                 </a>
                 <nav className="nav-links">
-                    <NavLink className='nav-btn' to='/'><span className='nav-icon'><i className="fa-solid fa-house"></i></span><span className='nav-name'>Home</span></NavLink>
+                    <NavLink onClick={onFull} className='nav-btn' to='/'><span className='nav-icon'><i className="fa-solid fa-house"></i></span><span className='nav-name'>Home</span></NavLink>
                     <a className='nav-btn'><span className='nav-icon'><i className="fa-solid fa-magnifying-glass"></i></span><span className='nav-name'>Search</span></a>
-                    <a className='nav-btn'><span className='nav-icon'><i className="fa-regular fa-compass"></i></span><span className='nav-name' >Explore</span></a>
+                    <a onClick={onFull} className='nav-btn'><span className='nav-icon'><i className="fa-regular fa-compass"></i></span><span className='nav-name' >Explore</span></a>
                     {/* <div className="btns-dif-spacing"> */}
-                    <NavLink className='nav-btn messages' ><span className='nav-icon'><i className="fa-brands fa-facebook-messenger"></i></span><span className='nav-name'>Messages</span></NavLink>
+                    <NavLink onClick={onMessages} className='nav-btn messages' to='/inbox'><span className='nav-icon'><i className="fa-brands fa-facebook-messenger"></i></span>{newMessage && newMessage.review ? <span className='new-msg'></span> : null}<span className='nav-name'>Messages</span></NavLink>
                     <a className='nav-btn notifications n'><span className='nav-icon'><i className="fa-regular fa-heart"></i></span><span className='nav-name'>Notifications</span></a>
 
                     <a onClick={toggleModal} className='nav-btn create mobile'><span className='nav-icon'><i className="fa-regular fa-square-plus"></i></span><span className='nav-name' >Create</span></a>
                     {isModalOpen && <CreateModal />}
-                    <NavLink className='nav-btn profile'
+                    <NavLink onClick={onFull} className='nav-btn profile'
                         to={user._id}
                     >
                         <span className='nav-icon'>
